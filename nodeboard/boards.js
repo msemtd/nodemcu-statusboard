f8bd12288f910ec53cf01dc13023bff532945a96
Michael Erskine <michael.erskine@ketech.com>
2016-03-08 16:30:29 +0000

    outline for a boards module

---
+++ nodeboard/boards.js
@@ -0,0 +1,26 @@
+// boards.js - status board information
+
+boardvalues = [0x00, 0x00];
+
+// TODO load and save with fs.writeFile('message.txt', 'Hello Node.js', 'utf8', callback);
+function getBoardVal(idx) {
+    
+}
+
+function setBoardVal(idx, newval) {
+    
+}
+
+function load() {
+    
+}
+
+function save() {
+    
+}
+
+exports.getBoardVal = getBoardVal;
+exports.setBoardVal = setBoardVal;
+exports.load = load;
+exports.save = save;
+
